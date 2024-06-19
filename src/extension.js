a556f4bd0e7b0a1ab5e6147458c95416cf77cd21
Xterionix <72647213+Xterionix@users.noreply.github.com>
2024-06-19 11:57:10 +0500

    - Change script versions to reflect 1.21.0.3

--- src/extension.js
+++ src/extension.js
@@ -20,8 +20,8 @@ const appData = process.env.APPDATA
 const downloadsFolder = path.join(os.homedir(), 'Downloads')
 
 const scriptVersion = {
-	"@minecraft/server": ["1.0.0", "1.1.0", "1.2.0", "1.3.0", "1.4.0", "1.5.0", "1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0", "1.10.0", "1.11.0-beta", "1.11.0", "1.12.0-beta"],
-	"@minecraft/server-ui": ["1.0.0", "1.1.0", "1.2.0-beta", "1.2.0", "1.3.0-beta"],
+	"@minecraft/server": ["1.0.0", "1.1.0", "1.2.0", "1.3.0", "1.4.0", "1.5.0", "1.6.0", "1.7.0", "1.8.0", "1.9.0", "1.10.0", "1.10.0", "1.11.0", "1.12.0-beta"],
+	"@minecraft/server-ui": ["1.0.0", "1.1.0", "1.2.0-beta"],
 	"@minecraft/common": ["1.2.0", "1.1.0", "1.0.0"]
 }
 
