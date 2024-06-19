23aa5dbc989750c9c3153c259e099c2c77cf04f8
Xterionix <72647213+Xterionix@users.noreply.github.com>
2024-06-19 11:55:10 +0500

    - Remake project switcher - Add support for switching to bp only projects (untested)

--- src/extension.js
+++ src/extension.js
@@ -13,7 +13,6 @@ const archiver = require('archiver')
 // TODO: Presets
 // TODO: More dynamic autocomplete
 // TODO: Improve cache system
-// TODO: Improve project switcher including adding support for opening bp/rp only projects
 // TODO: Add support for regolith exports
 // TODO: Add support for new project 
 
@@ -184,92 +183,77 @@ async function activate(context) {
 	const projectSwitcherCommand = vscode.commands.registerCommand('bedrocken.switch_projects', async () => {
 
 		try {
+
 			const root = vscode.workspace.getConfiguration('bedrocken').get('folders', [`${appData.replace(/\\/g, '').replace('Roaming', '')}//Local/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang`])
 			const options = []
 
-			const manifests = {
-				// 'something [bp]' : 'u2323-523254-2323-12af-g5a313'
-			}
-			const rpManifests = {
-				// 'u2323-523254-2323-12af-g5a313' : 'something [rp]'
-			}
+			/**
+			 * @typedef PackData
+			 * @prop {string} name
+			 * @prop {string} bp 
+			 * @prop {string} bpUuid 
+			 * @prop {string} rp 
+			 * @prop {string} rpUuid 
+			 */
 
-			const locations = {
-				// something [p] : c:/yser/saweea/a/e/a/e/ea/e/
-			}
+			/**
+			 * @type {Object<string,PackData>}
+			 */
+			const allData = {}
 
-			root.forEach(location => {
+			for (const location of root) {
 
 				const bpDirectory = path.join(location, "development_behavior_packs")
 				const rpDirectory = path.join(location, "development_resource_packs")
 
-				if (!fs.existsSync(bpDirectory)) {
-					vscode.window.showErrorMessage("Could not find development_behavior_packs")
-					return;
+				const bpFiles = await fs.promises.readdir(bpDirectory, { withFileTypes: true })
+
+				for (const bpFile of bpFiles) {
+					if (bpFile.isDirectory()) {
+						options.push(bpFile.name)
+						const manifest = parse((await fs.promises.readFile(path.join(bpFile.path, bpFile.name, 'manifest.json'))).toString())
+						allData[bpFile.name] = { name: bpFile.name, bp: path.join(bpFile.path, bpFile.name), bpUuid: manifest?.header?.uuid || '', rp: '', rpUuid: manifest?.dependencies?.filter(x => Object.hasOwn(x, 'uuid'))[0]?.uuid || '' }
+					}
 				}
 
-				if (!fs.existsSync(rpDirectory)) {
-					vscode.window.showErrorMessage("Could not find development_resource_packs")
-					return;
+				const rpfiles = await fs.promises.readdir(rpDirectory, { withFileTypes: true })
+
+				for (const rpFile of rpfiles) {
+					if (rpFile.isDirectory()) {
+						const manifest = parse((await fs.promises.readFile(path.join(rpFile.path, rpFile.name, 'manifest.json'))).toString())
+						const match = Object.values(allData).filter(packData => packData.rpUuid === manifest?.header?.uuid)[0]
+						if (match) {
+							allData[match.name].rp = path.join(rpFile.path, rpFile.name)
+						}
+					}
 				}
 
-				const files = fs.readdirSync(bpDirectory)
-
-				files.forEach(file => {
-					if (!fs.statSync(path.join(bpDirectory, file)).isDirectory()) return;
-					options.push(file)
-					locations[file] = path.join(bpDirectory, file)
-					try {
-						manifests[file] = parse(fs.readFileSync(path.join(bpDirectory, file, 'manifest.json'), 'utf-8')).dependencies.filter(obj => obj.hasOwnProperty('uuid'))[0].uuid
-					} catch (error) {
-						console.log('Malformed manifest.json for ' + file)
-					}
-				})
-
-				const rpfiles = fs.readdirSync(rpDirectory)
-
-				rpfiles.forEach(file => {
-					if (!fs.statSync(path.join(rpDirectory, file)).isDirectory()) return;
-					locations[file] = path.join(rpDirectory, file)
-					try {
-						rpManifests[parse(fs.readFileSync(path.join(rpDirectory, file, 'manifest.json'), 'utf-8')).header.uuid] = file
-					} catch (error) {
-						console.log('Malformed manifest.json for ' + file)
-					}
-				})
-
-			})
-
-			if (options.length == 0) {
-				vscode.window.showErrorMessage('No directories found')
-				return;
 			}
+
+			if (options.length == 0) { vscode.window.showErrorMessage('No directories found'); return }
+			
 			vscode.window.showQuickPick(options.sort()).then(async selectedOption => {
 				if (selectedOption) {
-					const existingWorkspaceFile = path.join(context.extensionPath, 'data/workspaces', selectedOption.replace(' [BP]', '').replace(' [RP]', '').toLowerCase() + '.code-workspace')
-					if (fs.existsSync(existingWorkspaceFile)) {
-						vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(existingWorkspaceFile), false)
-					} else {
+					const workspaceFile = path.join(context.extensionPath, 'data/workspaces', selectedOption.replace(' [BP]', '').replace(' [RP]', '').toLowerCase() + '.code-workspace')
+					if (fs.existsSync(workspaceFile)) vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(workspaceFile), false)
+					else {
 						const data = {
-							"folders": [
+							folders: [
 								{
-									"path": locations[selectedOption].replace(/\\/g, '/')
-								},
-								{
-									"path": locations[rpManifests[manifests[selectedOption]]].replace(/\\/g, '/')
+									path: allData[selectedOption].bp.replace(/\\/g, '/')
 								}
 							],
-							"settings": {}
+							settings: {}
 						}
-						await fs.promises.writeFile(existingWorkspaceFile, JSON.stringify(data, null, 4))
-						await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(existingWorkspaceFile), false)
+						if (allData[selectedOption].rp) data.folders.push({path: allData[selectedOption].rp.replace(/\\/g, '/')})
+						await fs.promises.writeFile(workspaceFile, JSON.stringify(data, null, 4))
+						await vscode.commands.executeCommand('vscode.openFolder', vscode.Uri.file(workspaceFile), false)
 					}
 				}
 			});
 		} catch (error) {
 			console.log(error)
 		}
-
 
 	})
 
@@ -641,7 +625,7 @@ async function activate(context) {
 			if (typeof value == 'string') value = [value];
 			const line = document.getText(new vscode.Range(new vscode.Position(position.line, 0), position)).trim()
 			value.forEach(x => {
-				const item =new vscode.CompletionItem(x, vscode.CompletionItemKind.Enum)
+				const item = new vscode.CompletionItem(x, vscode.CompletionItemKind.Enum)
 				let overlapLength = 0;
 				for (let i = 1; i <= x.length; i++) {
 					if (line.endsWith(x.slice(0, i))) {
@@ -649,7 +633,7 @@ async function activate(context) {
 					}
 				}
 				item.sortText = '0'
-				item.range= new vscode.Range(new vscode.Position(position.line, position.character - overlapLength), position);
+				item.range = new vscode.Range(new vscode.Position(position.line, position.character - overlapLength), position);
 				suggestions.push(item)
 			})
 			return suggestions
