750b55e10f063ddecc942a5b9317b7a9b2ce4497
Xterionix <72647213+Xterionix@users.noreply.github.com>
2024-06-19 21:10:05 +0500

    - Add entity property caching

--- src/sub/cacheSystem.js
+++ src/sub/cacheSystem.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const fs = require('fs')
-const {glob} = require('glob');
+const { glob } = require('glob');
 const { parse } = require('jsonc-parser')
 
 class CacheSystem {
@@ -9,7 +9,11 @@ class CacheSystem {
         entity: {
             ids: [],
             spawnable_ids: [],
-            rideable_ids: []
+            rideable_ids: [],
+            boolean_property: [],
+            integer_property: [],
+            float_property: [],
+            enum_property: []
         },
         item: {
             ids: [],
@@ -54,9 +58,9 @@ class CacheSystem {
      * @param {string} pattern
      */
     async processGlob(folderPath, pattern, type) {
-        
-        const files = await glob.glob(pattern, {cwd:folderPath })
-        
+
+        const files = await glob.glob(pattern, { cwd: folderPath })
+
         switch (type) {
             case 'loot_table': this.#cache.loot_tables.push(...files.map(x => x.replace(/\\/g, '/')))
                 break;
@@ -109,7 +113,18 @@ class CacheSystem {
 
     }
 
-    #processLoottables() {}
+    resetEntityCache() {
+        this.#cache.entity = {
+            ids: [],
+            spawnable_ids: [],
+            rideable_ids: [],
+            boolean_property: [],
+            integer_property: [],
+            float_property: [],
+            enum_property: []
+        }
+    }
+
     #processFeaturefile(json) {
         const identifier = json["minecraft:weighted_random_feature"]?.["description"]?.["identifier"] || json["minecraft:aggregate_feature"]?.["description"]?.["identifier"] || json["minecraft:cave_carver_feature"]?.["description"]?.["identifier"] || json["minecraft:fossil_feature"]?.["description"]?.["identifier"] || json["minecraft:geode_feature"]?.["description"]?.["identifier"] || json["minecraft:growing_plant_feature"]?.["description"]?.["identifier"] || json["minecraft:multiface_feature"]?.["description"]?.["identifier"] || json["minecraft:nether_cave_carver_feature"]?.["description"]?.["identifier"] || json["minecraft:ore_feature"]?.["description"]?.["identifier"] || json["minecraft:partially_exposed_blob_feature"]?.["description"]?.["identifier"] || json["minecraft:scatter_feature"]?.["description"]?.["identifier"] || json["minecraft:search_feature"]?.["description"]?.["identifier"] || json["minecraft:sequence_feature"]?.["description"]?.["identifier"] || json["minecraft:single_block_feature"]?.["description"]?.["identifier"] || json["minecraft:snap_to_surface_feature"]?.["description"]?.["identifier"] || json["minecraft:structure_template_feature"]?.["description"]?.["identifier"] || json["minecraft:surface_relative_threshold_feature"]?.["description"]?.["identifier"] || json["minecraft:tree_feature"]?.["description"]?.["identifier"] || json["minecraft:underwater_cave_carver_feature"]?.["description"]?.["identifier"] || json["minecraft:vegetation_patch_feature"]?.["description"]?.["identifier"]
         if (!identifier || identifier.includes('minecraft:')) return;
@@ -132,6 +147,22 @@ class CacheSystem {
         this.#cache.entity.ids.push(identifier)
         if (text.includes('minecraft:rideable')) this.#cache.entity.rideable_ids.push(identifier)
         if (description["is_spawnable"]) this.#cache.entity.spawnable_ids.push(identifier)
+        if (!description["properties"]) return;
+        const properties = description["properties"]
+        for (const property in properties) {
+            const type = properties[property]['type']
+            if (type) continue;
+            switch (type) {
+                case 'bool': this.#cache.entity.boolean_property.push(property)
+                    break;
+                case 'int': this.#cache.entity.integer_property.push(property)
+                    break;
+                case 'float': this.#cache.entity.float_property.push(property)
+                    break;
+                case 'enum': this.#cache.entity.enum_property.push({id: property, values: properties[property]['values'] || []})
+                    break;
+            }
+        }
     }
     #proceessItemFile(json) {
         const identifier = json["minecraft:item"]["description"]["identifier"]
