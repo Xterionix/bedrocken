e85d14385b80b0c5c2d9c9da2a375d6799de9025
Xterionix <72647213+Xterionix@users.noreply.github.com>
2024-07-03 17:06:53 +0500

    - Fix recipe suggestions including ''

--- src/completion/dynamicAutocomplete.js
+++ src/completion/dynamicAutocomplete.js
@@ -606,7 +606,7 @@ function createJsonProvider(system) {
                         identifier: fileBasedIdentifier
                     },
                     key: {
-                        property: jsonInDoc["minecraft:recipe_shaped"] ? (jsonInDoc["minecraft:recipe_shaped"]?.["pattern"] ? (jsonInDoc["minecraft:recipe_shaped"]["pattern"].length >= 1 ? Array.from(new Set(jsonInDoc["minecraft:recipe_shaped"]["pattern"].join('').split(''))) : []) : []) : [],
+                        property: jsonInDoc["minecraft:recipe_shaped"] ? (jsonInDoc["minecraft:recipe_shaped"]?.["pattern"] ? (jsonInDoc["minecraft:recipe_shaped"]["pattern"].length >= 1 ? Array.from(new Set(jsonInDoc["minecraft:recipe_shaped"]["pattern"].join('').split('').filter(x => x !== ''))) : []) : []) : [],
                         item: allItems
                     },
                     unlock: {
