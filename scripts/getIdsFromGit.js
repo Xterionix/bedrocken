1d48472c79736639b05e846197bd4ed6457f4637
Xterionix <xterionyt@gmail.com>
2024-12-08 20:28:22 +0500

    - Add script to fetch data from vp repo

---
+++ scripts/getIdsFromGit.js
@@ -0,0 +1,29 @@
+const https = require('https');
+const fs = require('fs');
+
+const entityFile = 'https://raw.githubusercontent.com/Mojang/bedrock-samples/main/metadata/vanilladata_modules/mojang-entities.json';
+const itemFile = 'https://raw.githubusercontent.com/Mojang/bedrock-samples/main/metadata/vanilladata_modules/mojang-items.json';
+const blockFile = 'https://raw.githubusercontent.com/Mojang/bedrock-samples/main/metadata/vanilladata_modules/mojang-blocks.json';
+
+const urls = [entityFile, itemFile, blockFile];
+
+urls.forEach(url => {
+    https.get(url, (response) => {
+        let data = '';
+
+        response.on('data', (chunk) => {
+            data += chunk;
+        });
+
+        response.on('end', () => {
+            try {
+                const jsonData = JSON.parse(data);
+                fs.writeFileSync(`./${url.split('/').pop().slice(0, -5)}.txt`, jsonData['data_items'].map(entry => "\"" + entry.name + "\"").join(','))
+            } catch (error) {
+                console.error('Error parsing JSON:', error);
+            }
+        });
+    }).on('error', (err) => {
+        console.error('Error fetching the JSON file:', err);
+    });
+});
