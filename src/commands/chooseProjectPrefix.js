2002431c8725b004d7d8451f57cdc76175cb41c4
Xterionix <xterionyt@gmail.com>
2025-01-11 19:11:03 +0500

    - Update todo

--- src/commands/chooseProjectPrefix.js
+++ src/commands/chooseProjectPrefix.js
@@ -5,6 +5,7 @@ const prefixForm = new Form([{ label: 'Project Prefix', description: 'project_pr
 
 async function chooseProjectPrefix() {
 
+    //TODO: Add syncing between installs
     const value = (await prefixForm.show())[0]
     vscode.workspace.getConfiguration('bedrocken').update('projectPrefix', value, vscode.ConfigurationTarget.Workspace)
     vscode.window.setStatusBarMessage('Project prefix set to ' + value, 2000)
